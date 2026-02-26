8918d53beea663ff973019268877462f0256d40a
Trương Trần Anh Minh <minhtruong300605@gmail.com>
2026-02-26 13:45:35 +0700

    :update

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,7 +2,7 @@ function About() {
   return (
     <div className="about">
       <h1>Welcome to the About Page</h1>
-      <p>This is the about page of our React application.</p>
+      <p>This is the about page is developed by Minh</p>
     </div>
   );
 }   
